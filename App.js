66fd04e9ee6b1483c42187ea8ed6dafe0b800f74
pratik-patel <pratik_patel@live.in>
2020-07-16 14:44:55 +0530

    Added global exception handlers

--- App.js
+++ App.js
@@ -4,6 +4,30 @@ import { StyleSheet, View, Button, FlatList, Text, StatusBar, SafeAreaView } fro
 import BluetoothStatus from './components/BluetoothStatus';
 import BluetoothManager from './components/BluetoothManager';
 import AudioToDecible from './components/AudioToDecible';
+import { setJSExceptionHandler, setNativeExceptionHandler } from 'react-native-exception-handler';
+
+const errorHandler = (e, isFatal) => {
+  if (isFatal) {
+    Alert.alert(
+        'Unexpected error occurred',
+        `
+        Error: ${(isFatal) ? 'Fatal:' : ''} ${e.name} ${e.message}
+
+        We have reported this to our team ! Please close the app and start again!
+        `,
+      [{
+        text: 'Close'
+      }]
+    );
+  }
+  console.log('setJSExceptionHandler '+e); // So that we can see it in the ADB logs in case of Android if needed  
+};
+
+setJSExceptionHandler(errorHandler, true);
+
+setNativeExceptionHandler((errorString) => {
+    console.log('setNativeExceptionHandler '+errorString);
+});
 
 export default function App() {
   return (
